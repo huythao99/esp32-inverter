3f447b1e67558ace1250e3f77958324303a48429
andrew99 <earlbosworth159@gmail.com>
2026-09-24 13:14:37 +0700

    disable debug

--- src/config.h
+++ src/config.h
@@ -8,7 +8,7 @@
 // Debug logging. Set DEBUG to 0 for production to compile out all USB-serial
 // debug output (removes ~90 blocking Serial.print calls from the hot paths).
 // Note: this only affects the USB Serial; the STM32 link (testSerial) is untouched.
-#define DEBUG 1
+#define DEBUG 0
 #if DEBUG
   #define DBG_PRINT(...)   Serial.print(__VA_ARGS__)
   #define DBG_PRINTLN(...) Serial.println(__VA_ARGS__)
